bb73b5191f5093714f7729129e5234eb4813d735
Sabbadini10 <matiassabbadini24@gmail.com>
2023-06-03 19:48:00 -0300

    bug(/):error solved

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
