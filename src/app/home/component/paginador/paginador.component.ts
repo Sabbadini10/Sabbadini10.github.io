7f52cde18105c6c7263c4e92d26d10f07576ee89
Sabbadini10 <matiassabbadini24@gmail.com>
2023-06-19 20:59:43 -0300

    add(paginador):add filter price

--- src/app/home/component/paginador/paginador.component.ts
+++ src/app/home/component/paginador/paginador.component.ts
@@ -1,121 +1,124 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PaginadorService } from './services/paginador.service';
-import { json } from 'express';
-
 
 @Component({
   selector: 'app-paginador',
   templateUrl: './paginador.component.html',
-  styleUrls: ['./paginador.component.css']
+  styleUrls: ['./paginador.component.css'],
 })
 export class PaginadorComponent implements OnInit {
-
-  imagenes: any[] = [];
-   _data: any[] = [];// Array para almacenar los productos
-   message: number[] = [];
+  imagenes: any[] = []; // Array para almacenar las imagenes
+  _data: any[] = []; // Array para almacenar los productos consumidos de la api
   name: string = '';
-  cost:  any[] = [];
-  productosHighFiltrados: any;
-  productosLowFiltrados: any;
-  constructor(private paginadorService: PaginadorService){}
-
+  puntos: number[] = []; // Array para almacenar los puntos
+  productosHighFiltrados: number[] = []; // Array para almacenar los productos caros
+  productosLowFiltrados: number[] = []; // Array para almacenar los productos baratos
+  productosFiltrados: any[] = [];
+  allProducts: any[] = []; //
   visibleProducts: any[] = []; // Array para almacenar los productos visibles
+  visibleProducts2: any[] = []; // Array para almacenar los productos visibles
   productsPerPage = 16; // Cantidad de productos que se mostrarán por página
-  nextProducts: any[] = []; // Array para almacenar los productos visibles
-  nextPerPage = 32; // Cantidad de productos que se mostrarán por página
-  
-  filteredProducts: any[]= [];
-  priceFilter: number = 250;
+  filteredProducts: any[] = [];
+  filteredDataProducts: Array<Object> = [];
+  filtroPrecioActivo: boolean = false;
+  filtroPrecioBarato: boolean = false;
+  filtroPrecioCaro: boolean = false;
 
-   ngOnInit(): void {
+  constructor(private paginadorService: PaginadorService) {}
+
+  ngOnInit(): void {
+    this.paginadorService.getData().subscribe((res: any) => {
+      this._data = res;
+      this.allProducts = this._data;
+    });
 
     this.paginadorService.getData().subscribe((res: any) => {
-      this._data = res
+      this._data = res;
       this.visibleProducts = this._data.slice(0, this.productsPerPage);
-      })
+    });
 
+    this.paginadorService.getData().subscribe((res: any) => {
+      this._data = res;
+      this.visibleProducts2 = this._data.slice(16, this.productsPerPage);
+    });
 
     this.paginadorService.getData().subscribe((res: any) => {
       this.imagenes = res;
-     console.log(this.imagenes)
-      })
+      console.log(this.imagenes);
+    });
+  }
 
-
-      this.paginadorService.getData().subscribe((res: any) => {
-        const myDinero = 500;
-        this.cost = res
-        this.filteredProducts = this.cost.map(item => item.cost);
-         this.productosHighFiltrados = this.filteredProducts.filter(producto => producto >= myDinero);
-         this.productosLowFiltrados = this.filteredProducts.filter(producto => producto <= myDinero);
-        console.log('>>><<<>>>'+ this.productosHighFiltrados)
-        console.log('>>><<<>>>'+ this.productosLowFiltrados)
-        })
-
-
-
-   }
-
-
-   showHighestProducts() {
-    const myDinero = 1000;
-    if(this.productosHighFiltrados >= myDinero){
-      return this.productosHighFiltrados;
-    }else {
-      return "NO TE ALCANZA"
-     
-    }
+  showHighestProducts() {
+    const myDinero = 500;
+    return this.allProducts.filter((producto) => producto.cost >= myDinero);
   }
 
   showLowestProducts() {
-    const myDinero = 500;
-    if(this.productosLowFiltrados <= myDinero){
-     return this.productosLowFiltrados;
-    }else {
-      return "NO TE ALCANZA"
+    const myDinero = 300;
+    return this.allProducts.filter((producto) => producto.cost <= myDinero);
+  }
+
+  toggleFiltroPrecioBajo() {
+    if (this.filtroPrecioBarato) {
+      this.filtroPrecioActivo = false;
+      this.filtroPrecioBarato = false;
+      this.productosFiltrados = this.allProducts; // Restablecer la lista de productos filtrados
+    } else {
+      this.filtroPrecioActivo = true;
+      this.filtroPrecioCaro = false;
+      this.filtroPrecioBarato = true;
+      this.productosFiltrados = this.showLowestProducts(); // Aplicar el filtrado por precio
     }
   }
 
-
-   first1: number = 16;
-
-    rows1: number = 16;
-
-    first2: number = 32;
-
-    rows2: number = 32;
-
-
-    totalRecords: number = 32;
-
-    options = [
-        { label: 16, value: 16 },
-        { label: 32, value:32 },
-    ];
-
-    onPageChange1(event: any) {
-        this.first1 = event.first;
-        this.rows1 = event.rows;
+  toggleFiltroPrecioCaro() {
+    if (this.filtroPrecioCaro) {
+      this.filtroPrecioActivo = false;
+      this.filtroPrecioCaro = false;
+      this.productosFiltrados = this.allProducts; // Restablecer la lista de productos filtrados
+    } else {
+      this.filtroPrecioActivo = true;
+      this.filtroPrecioBarato = false;
+      this.filtroPrecioCaro = true;
+      this.productosFiltrados = this.showHighestProducts(); // Aplicar el filtrado por precio
     }
+  }
 
-    onPageChange2(event: any) {
-        this.first2 = event.first;
-        this.rows2 = event.rows;
-    }
+  first1: number = 16;
+  rows1: number = 16;
+  first2: number = 32;
+  rows2: number = 32;
 
-    isClicked1: boolean = false;
-    isClicked2: boolean = false;
-    isClicked3: boolean = false;
+  totalRecords: number = 32;
 
-    changeColor1() {
-      this.isClicked1 = !this.isClicked1;
-    }
+  options = [
+    { label: 16, value: 16 },
+    { label: 32, value: 32 },
+  ];
 
-    changeColor2() {
-      this.isClicked2 = !this.isClicked2;
-    }
+  onPageChange1(event: any) {
+    this.first1 = event.first;
+    this.rows1 = event.rows;
+  }
 
-    changeColor3() {
-      this.isClicked3 = !this.isClicked3;
-    }
+  onPageChange2(event: any) {
+    this.first2 = event.first;
+    this.rows2 = event.rows;
+  }
 
+  isClicked1: boolean = false;
+  isClicked2: boolean = false;
+  isClicked3: boolean = false;
+
+  changeColor1() {
+    this.isClicked1 = !this.isClicked1;
+  }
+
+  changeColor2() {
+    this.isClicked2 = !this.isClicked2;
+  }
+
+  changeColor3() {
+    this.isClicked3 = !this.isClicked3;
+  }
 }
