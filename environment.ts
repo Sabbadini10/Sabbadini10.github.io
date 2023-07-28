9ffbd3b13dd0d94ba473519bbb8b2604fab32195
Matias Sabbadini <matiassabbadini24@gmail.com>
2023-07-28 14:54:33 -0300

    change enviroments

--- environment.ts
+++ environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: false,
+  production: false ,
   MY_TOKEN:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDc4YTk2OWFlNDM0ODAwMjBmYjI1ZTEiLCJpYXQiOjE2ODU2MjkyODl9.VNDu-cpdPQsLMcmKYzMkh2Ndts9E0EZZ-HycUA6sR3k'
 };
+
+  
